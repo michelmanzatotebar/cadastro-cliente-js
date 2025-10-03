de956180b3b9def603a709237223bb4f6e455a46
michelmanzatotebar <zsouta22@gmail.com>
2025-10-02 20:23:57 -0400

    Configurado yaml para upar no docker hub

--- cadastro-cliente/db.js
+++ cadastro-cliente/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cadastro_clientes',
-    password: 'michel',
-    port: 5432,
+    user: process.env.PGUSER || 'postgres',
+    host: process.env.PGHOST || 'localhost',
+    database: process.env.PGDATABASE || 'cadastro_clientes',
+    password: process.env.PGPASSWORD || 'michel',
+    port: process.env.PGPORT || 5432,
 });
 
 module.exports = pool;
